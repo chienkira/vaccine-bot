53ae6e2104f3533b035a151d62456316a15e01f1
chien <chien@fantas-tech.co.jp>
2021-08-19 21:51:31 +0900

    small fix

--- health.js
+++ health.js
@@ -6,6 +6,7 @@ require('dotenv').config({ path: __dirname + '/.env' });
 const { startBrowser, login, sendMessage } = require('./notification');
 
 const main = async (argv) => {
+  console.debug(``)
   const { browser, page } = await startBrowser();
   await login(page);
   await sendMessage(page, '100002425288918', `I'm ALIVE`);
